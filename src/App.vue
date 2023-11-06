129f4f976be787e7ebd25facd47fc96daf9fa294
victorgis <essangvictor@gmail.com>
2023-11-06 11:54:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView />
+</template>
+
+<script>
+import { RouterView } from "vue-router";
+
+export default {
+  name: "App",
+  components: {
+    RouterView,
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="parent">
+    <Header />
+    <div class="main">Test App</div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  name: "JobsiteHomeView",
+  components: { Header, Footer },
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
