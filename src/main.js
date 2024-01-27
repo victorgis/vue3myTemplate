89434a95eee0d23dc10dec84688e8ee1d90ff469
Victor Essang <47665722+victorgis@users.noreply.github.com>
2024-01-27 08:51:24 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ToastPlugin from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-bootstrap.css'
+import 'vue-toast-notification/dist/theme-sugar.css'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
+// this.$toast.error(message)
+// this.$toast.warning(message)
+// this.$toast.info(message)
+// this.$toast.default(message)
+
 app.use(router)
+app.use(ToastPlugin, {
+  position: 'top-right'
+})
 
 app.mount('#app')
